9d942beb8dfcacd829b2fd42416d5489ffbe21fe
Max Rohrer <mxrohrer@gmail.com>
2016-12-26 18:41:55 -0800

    set up random word selector function for hangman

--- hangman/script.js
+++ hangman/script.js
@@ -1,5 +1,17 @@
-$(document).ready(function{
+$(document).ready(function(){
+  var wordList = ['apple', 'sasquatch', 'fart']; //array to hold random words
+
+  var randWord = ""; //variable to store random word
+
+  function wordSelect(wordList){
+    var randNum = Math.floor(Math.random()*wordList.length);
+    randWord = wordList[randNum];
+  }
+  
+
+  //a reset function to present all letters as unclicked and select a random word to display blanks for
+
   for(var i = 1; i<=26; i++){
-    $(`${i}`).append(`<img src='img/${i}.jpg' alt='letter ${i} of the alphabet' class="letterPic">`)
+    $(`${i}`).append(`<img src='img/${i}.png' alt='letter ${i} of the alphabet' class="letterPic">`);
   }
 });
